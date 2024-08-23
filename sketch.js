0d53c522c31e51bec855453f5540f7af9ec0ef92
YB-SN <00001237091172sp@al.educacao.sp.gov.br>
2024-08-23 20:35:39 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+nction setup() {
+    createCanvas(600, 600);
+    background("purple")
+  }
+  
+  function draw() {
+    
+    stroke("blue");
+     fill("red");
+    
+     //console.log(mouseIsPressed);
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+    
+  }
+                                                             
